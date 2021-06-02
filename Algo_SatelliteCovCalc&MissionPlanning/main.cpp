d08d5957c021e7ece2f5343385eb8557d19454e5
airlariken <158287382@qq.com>
2021-06-02 09:02:10 +0800

    Initial Commit

---
+++ Algo_SatelliteCovCalc&MissionPlanning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algo_SatelliteCovCalc&MissionPlanning
+//
+//  Created by 陈梓玮 on 2021/6/2.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
